6121e957a512513f0123560b7ba0b0ecc145f265
Tal Sliwowicz <tsliwowicz@gmail.com>
2014-01-24 19:41:57 +0200

    model template

---
+++ fartroulette/src/main/java/org/gamejam/gc/fartroulette/model/CachedModel.java
@@ -0,0 +1,34 @@
+package org.gamejam.gc.fartroulette.model;
+
+public class CachedModel {
+	
+	public static class ElevatorData {
+		/*
+		 * ID
+		 * 
+		 * state - before, selection, after, leaderboard
+		 * 
+		 * timer
+		 * 
+		 * farters - bets (users), animation
+		 * 
+		 * 
+		 * players - score
+		 * 
+		 */
+		
+	}
+	
+	public static class LeaderBoard {
+		/*
+		 * users -	score
+		 * 
+		 * 
+		 * 
+		 */
+		
+	}
+	
+	
+
+}

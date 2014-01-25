a8bb610cabfd959b9af3a1c3cd9d17862e048ad4
Tal Sliwowicz <tsliwowicz@gmail.com>
2014-01-25 10:27:21 +0200

    added slots to the model

--- fartroulette/src/main/java/org/gamejam/gc/fartroulette/model/ModelClasses.java
+++ fartroulette/src/main/java/org/gamejam/gc/fartroulette/model/ModelClasses.java
@@ -19,6 +19,7 @@ public class ModelClasses {
 		public Map<String, CharacterData> charactersData = new HashMap<String, ModelClasses.CharacterData>();
 		public ConcurrentMap<Integer, UserData> activeUsers;
 		public GameState gameState;
+		public List<Chars> currSlots;
 		
 		
 		@Override
@@ -99,11 +100,23 @@ public class ModelClasses {
 		ONE, TWO, THREE, NONE
 	}
 	
+	public static enum Chars {
+		Char_01, Char_02, Char_03, Char_04, Char_05, Char_06
+	}
+	
 	
 	public static class ElevatorData {
 		public long timeLeftForState = 0;
 		public GameState gameState = GameState.BEFORE;
 		public final ConcurrentMap<Integer, UserData> activeUsers = new ConcurrentHashMap<Integer, UserData>();
+		public final List<Chars> currSlots = new ArrayList<ModelClasses.Chars>(4);
+		
+		public ElevatorData() {
+			currSlots.add(Chars.Char_01);
+			currSlots.add(Chars.Char_02);
+			currSlots.add(Chars.Char_03);
+			currSlots.add(Chars.Char_06);
+		}
 		
 		
 		
@@ -120,6 +133,7 @@ public class ModelClasses {
 			model.activeUsers = activeUsers;
 			model.timeLeftForState = timeLeftForState;
 			model.gameState = gameState;
+			model.currSlots = currSlots;
 			
 			for (Entry<Integer, UserData> u: activeUsers.entrySet()) {
 				UserData user = u.getValue();

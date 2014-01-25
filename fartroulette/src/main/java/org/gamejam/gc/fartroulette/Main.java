0560ff9b26e07e607fb4fa59683e3906354f6af5
Tal Sliwowicz <tsliwowicz@gmail.com>
2014-01-25 11:08:50 +0200

    fixed log levels

--- fartroulette/src/main/java/org/gamejam/gc/fartroulette/Main.java
+++ fartroulette/src/main/java/org/gamejam/gc/fartroulette/Main.java
@@ -1,6 +1,8 @@
 package org.gamejam.gc.fartroulette;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 //test with http://localhost:8080/test/websocket.html
 public class Main 
@@ -8,6 +10,8 @@ public class Main
     public static void main( String[] args ) throws Exception
     {
     	BasicConfigurator.configure();
+    	Logger.getRootLogger().setLevel(Level.WARN);
+    	Logger.getLogger("org.gamejam").setLevel(Level.DEBUG);
     	 int port;
          if (args.length > 0) {
              port = Integer.parseInt(args[0]);
